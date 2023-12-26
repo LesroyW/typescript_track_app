a075108458d5c08e978bd276ed80c5aae381ee04
Lesroy Wedderburn <wlesroy98@outlook.com>
2023-12-26 18:04:13 +0000

    Inital build of tracks folder

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,10 +1,17 @@
+require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = '';
 mongoose.connect(mongoUri, {
@@ -19,8 +26,8 @@ mongoose.connection.on('error', (err) =>  {
     console.error('Error connecting to mongo', err);
 });
 
-app.get('/', (req, res) => {
-res.send('Hi there!');
+app.get('/', requireAuth, (req, res) => {
+res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
